85ec52d42f17f28de910cc3c60ffe0bf428d4501
florencia.marinio@davinci.edu.ar <florencia.marino@davinci.edu.ar>
2020-11-14 16:08:32 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,32 +1,74 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
+import axios from 'axios';
 
 
-class App extends Component
+class App extends React.Component
 {
-  render()
+  state = 
   {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    response: [],
+    estado: null
+  };
+
+  handlerFind(f)
+  {
+    var find = f.target.value;
+    this.setState({value : find});
+  }  
+
+  handlerButton = () => 
+  {
+    var buscar = this.state.value; 
+    if(buscar==null)
+    { 
+    axios.get( 'https://api.openbrewerydb.org/breweries')
+        .then( dato => {
+        console.log(dato.data); 
+          
+        
+        this.setState({
+          response: dato.data,
+          estado: true
+        }) 
+        console.log(this.state)
+    });
+    } 
+
   }
+
+
+  render () 
+  {
+
+    if(this.state.estado !== true )
+    {
+    return (
+      <div className="App">
+      <header className="App-header">
+      <div>
+        <div>
+            <p>Buscar cervecería por nombre</p>
+            <input type="text" name="name" placeholder="[a-z]" onChange={this.handlerFind.bind(this)}/>
+            <input type="button" value="Buscar" onClick={this.handlerButton.bind(this)}/>
+        </div>
+        <div>
+          <p>Nombre: {this.state.response.name}</p>
+          <p>Tipo de cerveceria: {this.state.response.brewery_type}</p>
+          <p>Direccion: {this.state.response.street}</p>
+          <p>Ciudad: {this.state.response.city}</p>
+          <p>Pais: {this.state.response.country}</p>
+          <p>Telefono: {this.state.response.phone}</p>
+          <p>Pagina Web: {this.state.response.website_url}</p>
+        </div>
+      </div>
+      </header>
+    </div>  
+    );
+    }
+  }  
+
 }
 
 export default App;
